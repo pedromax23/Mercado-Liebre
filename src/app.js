f5ef7400cf0a1cde96c8724650ba1f9ee7257047
pedromax23 <pedromorgade1234@gmail.com>
2023-11-23 20:28:22 -0300

    configuracion para render

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const path = require("path");
 
 app.use(express.static(path.resolve(__dirname, "../public")))
 
-app.listen(3000, () => console.log("Servidor corriendo en el puerto: http://localhost:3000/"))
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./views/index.html")))
 app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "./views/register.html")))
