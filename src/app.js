d3ec7de986a130dd292222e1edce6cdf0369577d
pedromax23 <pedromorgade1234@gmail.com>
2024-01-05 19:07:06 -0300

    Implementando EJS

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const express = require("express"); // Express
+const app = express();  // Ejecucion de express
+const path = require("path"); // Path
 
+// Configuracion EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+// Archivos estaticos
 app.use(express.static(path.resolve(__dirname, "../public")))
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./views/index.html")))
+const mainRouter = require('./routers/main-router')
+app.use('/', mainRouter)
+
 app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "./views/register.html")))
 app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "./views/login.html")))
